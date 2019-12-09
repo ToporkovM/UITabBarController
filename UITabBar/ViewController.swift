4e1a85d82ca51fa4288851f9d5c8ab78c8d6d21c
MaxT <toporkovmaksim7@gmail.com>
2019-12-09 17:32:07 +0700

    Initial Commit

---
+++ UITabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by max on 09.12.2019.
+//  Copyright © 2019 MaxT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
