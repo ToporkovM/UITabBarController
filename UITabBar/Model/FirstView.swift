d8a56f727a7193e6e4c6795ad8e36eb15f12969a
MaxT <toporkovmaksim7@gmail.com>
2019-12-10 19:32:53 +0700

    first commit

---
+++ UITabBar/Model/FirstView.swift
@@ -0,0 +1,21 @@
+//
+//  FirstView.swift
+//  UITabBar
+//
+//  Created by max on 09.12.2019.
+//  Copyright © 2019 MaxT. All rights reserved.
+//
+
+import UIKit
+
+class FirstView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UITabBar/SecondViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondViewController.swift
+//  UITabBar
+//
+//  Created by max on 10.12.2019.
+//  Copyright © 2019 MaxT. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override var title: String?
+}
